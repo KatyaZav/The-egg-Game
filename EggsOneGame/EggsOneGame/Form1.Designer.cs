a7d6ef571b568edba2d1b0971f4b00ad63ec67b2
KatyaZav <Party2015ru@mail.ru>
2021-06-23 18:28:09 +0500

    Form design

--- EggsOneGame/EggsOneGame/Form1.Designer.cs
+++ EggsOneGame/EggsOneGame/Form1.Designer.cs
@@ -28,13 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ballUpLeftFirst = new System.Windows.Forms.PictureBox();
+            this.BasketM = new System.Windows.Forms.PictureBox();
+            this.BasketU = new System.Windows.Forms.PictureBox();
+            this.BasketC = new System.Windows.Forms.PictureBox();
+            this.BasketR = new System.Windows.Forms.PictureBox();
+            this.ballUpLeftSecond = new System.Windows.Forms.PictureBox();
+            this.ballDownLeftFirst = new System.Windows.Forms.PictureBox();
+            this.ballDownLeftSecond = new System.Windows.Forms.PictureBox();
+            this.ballUpRightFirst = new System.Windows.Forms.PictureBox();
+            this.ballUpRightSecond = new System.Windows.Forms.PictureBox();
+            this.ballDownRightFirst = new System.Windows.Forms.PictureBox();
+            this.ballDownRightSecond = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpLeftFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpLeftSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownLeftFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownLeftSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpRightFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpRightSecond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownRightFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownRightSecond)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ballUpLeftFirst
+            // 
+            this.ballUpLeftFirst.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballUpLeftFirst.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballUpLeftFirst.Location = new System.Drawing.Point(25, 28);
+            this.ballUpLeftFirst.Name = "ballUpLeftFirst";
+            this.ballUpLeftFirst.Size = new System.Drawing.Size(96, 82);
+            this.ballUpLeftFirst.TabIndex = 4;
+            this.ballUpLeftFirst.TabStop = false;
+            this.ballUpLeftFirst.Visible = false;
+            // 
+            // BasketM
+            // 
+            this.BasketM.BackgroundImage = global::EggsOneGame.Properties.Resources.basket;
+            this.BasketM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BasketM.Location = new System.Drawing.Point(591, 375);
+            this.BasketM.Name = "BasketM";
+            this.BasketM.Size = new System.Drawing.Size(139, 122);
+            this.BasketM.TabIndex = 3;
+            this.BasketM.TabStop = false;
+            this.BasketM.Visible = false;
+            // 
+            // BasketU
+            // 
+            this.BasketU.BackgroundImage = global::EggsOneGame.Properties.Resources.basket;
+            this.BasketU.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BasketU.Location = new System.Drawing.Point(591, 195);
+            this.BasketU.Name = "BasketU";
+            this.BasketU.Size = new System.Drawing.Size(139, 122);
+            this.BasketU.TabIndex = 2;
+            this.BasketU.TabStop = false;
+            this.BasketU.Visible = false;
+            // 
+            // BasketC
+            // 
+            this.BasketC.BackgroundImage = global::EggsOneGame.Properties.Resources.basket;
+            this.BasketC.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BasketC.Location = new System.Drawing.Point(228, 375);
+            this.BasketC.Name = "BasketC";
+            this.BasketC.Size = new System.Drawing.Size(139, 122);
+            this.BasketC.TabIndex = 1;
+            this.BasketC.TabStop = false;
+            // 
+            // BasketR
+            // 
+            this.BasketR.BackgroundImage = global::EggsOneGame.Properties.Resources.basket;
+            this.BasketR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BasketR.Location = new System.Drawing.Point(228, 195);
+            this.BasketR.Name = "BasketR";
+            this.BasketR.Size = new System.Drawing.Size(139, 122);
+            this.BasketR.TabIndex = 0;
+            this.BasketR.TabStop = false;
+            this.BasketR.Visible = false;
+            // 
+            // ballUpLeftSecond
+            // 
+            this.ballUpLeftSecond.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballUpLeftSecond.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballUpLeftSecond.Location = new System.Drawing.Point(127, 107);
+            this.ballUpLeftSecond.Name = "ballUpLeftSecond";
+            this.ballUpLeftSecond.Size = new System.Drawing.Size(96, 82);
+            this.ballUpLeftSecond.TabIndex = 5;
+            this.ballUpLeftSecond.TabStop = false;
+            this.ballUpLeftSecond.Visible = false;
+            // 
+            // ballDownLeftFirst
+            // 
+            this.ballDownLeftFirst.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballDownLeftFirst.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballDownLeftFirst.Location = new System.Drawing.Point(12, 235);
+            this.ballDownLeftFirst.Name = "ballDownLeftFirst";
+            this.ballDownLeftFirst.Size = new System.Drawing.Size(96, 82);
+            this.ballDownLeftFirst.TabIndex = 6;
+            this.ballDownLeftFirst.TabStop = false;
+            this.ballDownLeftFirst.Visible = false;
+            // 
+            // ballDownLeftSecond
+            // 
+            this.ballDownLeftSecond.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballDownLeftSecond.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballDownLeftSecond.Location = new System.Drawing.Point(126, 293);
+            this.ballDownLeftSecond.Name = "ballDownLeftSecond";
+            this.ballDownLeftSecond.Size = new System.Drawing.Size(96, 82);
+            this.ballDownLeftSecond.TabIndex = 7;
+            this.ballDownLeftSecond.TabStop = false;
+            this.ballDownLeftSecond.Visible = false;
+            // 
+            // ballUpRightFirst
+            // 
+            this.ballUpRightFirst.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballUpRightFirst.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballUpRightFirst.Location = new System.Drawing.Point(841, 28);
+            this.ballUpRightFirst.Name = "ballUpRightFirst";
+            this.ballUpRightFirst.Size = new System.Drawing.Size(96, 82);
+            this.ballUpRightFirst.TabIndex = 8;
+            this.ballUpRightFirst.TabStop = false;
+            this.ballUpRightFirst.Visible = false;
+            // 
+            // ballUpRightSecond
+            // 
+            this.ballUpRightSecond.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballUpRightSecond.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballUpRightSecond.Location = new System.Drawing.Point(736, 107);
+            this.ballUpRightSecond.Name = "ballUpRightSecond";
+            this.ballUpRightSecond.Size = new System.Drawing.Size(96, 82);
+            this.ballUpRightSecond.TabIndex = 9;
+            this.ballUpRightSecond.TabStop = false;
+            this.ballUpRightSecond.Visible = false;
+            // 
+            // ballDownRightFirst
+            // 
+            this.ballDownRightFirst.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballDownRightFirst.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballDownRightFirst.Location = new System.Drawing.Point(841, 235);
+            this.ballDownRightFirst.Name = "ballDownRightFirst";
+            this.ballDownRightFirst.Size = new System.Drawing.Size(96, 82);
+            this.ballDownRightFirst.TabIndex = 10;
+            this.ballDownRightFirst.TabStop = false;
+            this.ballDownRightFirst.Visible = false;
+            // 
+            // ballDownRightSecond
+            // 
+            this.ballDownRightSecond.BackgroundImage = global::EggsOneGame.Properties.Resources.ball;
+            this.ballDownRightSecond.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ballDownRightSecond.Location = new System.Drawing.Point(736, 293);
+            this.ballDownRightSecond.Name = "ballDownRightSecond";
+            this.ballDownRightSecond.Size = new System.Drawing.Size(96, 82);
+            this.ballDownRightSecond.TabIndex = 11;
+            this.ballDownRightSecond.TabStop = false;
+            this.ballDownRightSecond.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(949, 540);
+            this.Controls.Add(this.ballDownRightSecond);
+            this.Controls.Add(this.ballDownRightFirst);
+            this.Controls.Add(this.ballUpRightSecond);
+            this.Controls.Add(this.ballUpRightFirst);
+            this.Controls.Add(this.ballDownLeftSecond);
+            this.Controls.Add(this.ballDownLeftFirst);
+            this.Controls.Add(this.ballUpLeftSecond);
+            this.Controls.Add(this.ballUpLeftFirst);
+            this.Controls.Add(this.BasketM);
+            this.Controls.Add(this.BasketU);
+            this.Controls.Add(this.BasketC);
+            this.Controls.Add(this.BasketR);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpLeftFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasketR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpLeftSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownLeftFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownLeftSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpRightFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballUpRightSecond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownRightFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballDownRightSecond)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox BasketR;
+        private System.Windows.Forms.PictureBox BasketC;
+        private System.Windows.Forms.PictureBox BasketU;
+        private System.Windows.Forms.PictureBox BasketM;
+        private System.Windows.Forms.PictureBox ballUpLeftFirst;
+        private System.Windows.Forms.PictureBox ballUpLeftSecond;
+        private System.Windows.Forms.PictureBox ballDownLeftFirst;
+        private System.Windows.Forms.PictureBox ballDownLeftSecond;
+        private System.Windows.Forms.PictureBox ballUpRightFirst;
+        private System.Windows.Forms.PictureBox ballUpRightSecond;
+        private System.Windows.Forms.PictureBox ballDownRightFirst;
+        private System.Windows.Forms.PictureBox ballDownRightSecond;
     }
 }
 
--- EggsOneGame/EggsOneGame/Form1.cs
+++ EggsOneGame/EggsOneGame/Form1.cs
@@ -12,9 +12,50 @@ namespace EggsOneGame
 {
     public partial class Form1 : Form
     {
+        enum Basket
+        {
+            topLeft,
+            downLeft,
+            topRight,
+            downRight
+        };
+
+        Basket BasketActive = Basket.downLeft; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case (Keys.R):
+                    BasketR.Visible = true;
+                    BasketC.Visible = false;
+                    BasketU.Visible = false;
+                    BasketM.Visible = false;
+                    break;
+                case (Keys.C):
+                    BasketR.Visible = false;
+                    BasketC.Visible = true;
+                    BasketU.Visible = false;
+                    BasketM.Visible = false;
+                    break;
+                case (Keys.U):
+                    BasketR.Visible = false;
+                    BasketC.Visible = false;
+                    BasketU.Visible = true;
+                    BasketM.Visible = false;
+                    break;
+                case (Keys.M):
+                    BasketR.Visible = false;
+                    BasketC.Visible = false;
+                    BasketU.Visible = false;
+                    BasketM.Visible = true;
+                    break;
+            }
         }
     }
 }
